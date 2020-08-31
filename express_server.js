17c35cdf6607e5d898ed6ba24bcfe4b09724cbe5
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-08-31 22:22:50 +0000

    Add a GET Route to Show the Form & setup body-parser

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -12,6 +15,10 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
